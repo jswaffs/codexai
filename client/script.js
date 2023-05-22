427ce0262274568abe857e88bf7d20b2f7ebc665
jswaffs <90722137+jswaffs@users.noreply.github.com>
2023-05-22 20:51:55 +0100

    Update script.js

--- client/script.js
+++ client/script.js
@@ -86,7 +86,7 @@ const handleSubmit = async (e) => {
     // messageDiv.innerHTML = "..."
     loader(messageDiv)
 
-    const response = await fetch('https://codexai-pge5.onrender.com', {
+    const response = await fetch('https://johnai-h40u.onrender.com', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
